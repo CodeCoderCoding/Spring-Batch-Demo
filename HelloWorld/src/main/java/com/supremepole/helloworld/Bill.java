d3565554576559ce8bb50b7b705ecfcfdc08e75f
CodeCoderCoding <kneil5778@gmail.com>
2022-10-18 20:56:24 +0800

    add HelloWorld module

---
+++ HelloWorld/src/main/java/com/supremepole/helloworld/Bill.java
@@ -0,0 +1,37 @@
+package com.supremepole.helloworld;
+
+public class Bill {
+
+    private int id = 1;
+    private String name = "";
+    private int amount = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String toString() {
+        return "id=" + getId() + ";name=" + getName() + ";amount=" + getAmount();
+    }
+
+}
---
+++ HelloWorld/src/main/java/com/supremepole/helloworld/JobLaunch.java
@@ -0,0 +1,24 @@
+package com.supremepole.helloworld;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class JobLaunch {
+
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("job.xml");
+        JobLauncher launcher = (JobLauncher) context.getBean("jobLauncher");
+        Job job = (Job) context.getBean("billJob");
+        try {
+            JobExecution result = launcher.run(job, new JobParameters());
+            System.out.println(result.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
