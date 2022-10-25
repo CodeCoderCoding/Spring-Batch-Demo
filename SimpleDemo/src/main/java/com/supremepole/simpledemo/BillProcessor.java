4fde3326fa509f6aff388c14bac97a5a0278e9c0
CodeCoderCoding <kneil5778@gmail.com>
2022-10-25 21:25:15 +0800

    update SimpleDemo module

---
+++ SimpleDemo/src/main/java/com/supremepole/simpledemo/BillProcessor.java
@@ -0,0 +1,12 @@
+package com.supremepole.simpledemo;
+
+import org.springframework.batch.item.ItemProcessor;
+
+public class BillProcessor implements ItemProcessor<Bill, Bill> {
+
+    public Bill process(Bill bill) {
+        System.out.println(bill.toString());
+        return bill;
+    }
+
+}
