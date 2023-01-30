19488ea6773fdf41e8fd63f41402c4fc9b5c6370
CodeCoderCoding <kneil5778@gmail.com>
2023-01-30 22:20:29 +0800

    update SimpleDemo

--- SimpleDemo/src/main/java/com/supremepole/simpledemo/JobLaunch.java
+++ SimpleDemo/src/main/java/com/supremepole/simpledemo/JobLaunch.java
@@ -3,9 +3,13 @@ package com.supremepole.simpledemo;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Date;
 
 public class JobLaunch {
 
@@ -14,8 +18,14 @@ public class JobLaunch {
         JobLauncher launcher = (JobLauncher) context.getBean("jobLauncher");
         Job job = (Job) context.getBean("billJob");
         try {
-            JobExecution result = launcher.run(job, new JobParameters());
-            System.out.println(result.toString());
+            JobParameters jobParameters=new JobParametersBuilder().addDate("date", new Date()).toJobParameters();
+            JobExecution jobExecution = launcher.run(job, jobParameters);
+            ExecutionContext executionContext=jobExecution.getExecutionContext();
+            System.out.println(executionContext);
+            executionContext.putString("executionContextKey", "executionContextValue");
+            jobExecution.setExecutionContext(executionContext);
+            System.out.println(executionContext);
+            System.out.println(jobExecution.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
